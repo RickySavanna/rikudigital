1cf5f67c0be24679194a0c8348b25d9c50ab9f4c
RickySavanna <91821314+RickySavanna@users.noreply.github.com>
2023-07-21 15:43:22 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -3,14 +3,14 @@
 		event.preventDefault();
 		const formData = new FormData(event.target);
   		const formDataArr = [...formData.entries()];
-		const reqData = {id: "a8adf968-2a74-43a0-b15b-9b1b000f5b86", data: {}};
+		const reqData = {id: "499cce7a-bada-4e52-8e17-4a05f1e85077", data: {}};
 		for (let i = 0; i < formDataArr.length; i++) {
 			const field = formDataArr[i];
 			reqData.data[field[0]] = field[1];
 		}
 		console.log(reqData);
 		console.log(JSON.stringify(reqData));
-		fetch('https://api.usestyle.ai/api/v1/send-email', {
+		fetch('', {
 			method: 'POST',
 			headers: {
 				'Accept': 'application/json',
